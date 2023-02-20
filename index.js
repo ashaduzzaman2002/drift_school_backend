1a223583916b474788a3755f4513e74936a9ea92
Ashaduzzaman Mondal <87015301+ashaduzzaman2002@users.noreply.github.com>
2023-02-20 16:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,12 @@ const socialRouter = require('./routes/social');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(origin: 'https://agumentik-test.netlify.app/'))
+app.use(cors())
 
 app.use('/api/user', userRouter);
 app.use('/api/lead', leadRouter);
 app.use('/api/social', socialRouter);
 
-// frontend
-app.use(express.static(__dirname + './frontend/build/index.html'))
-
-app.get('*', (req, res) => {
-  res.sendFile(
-    __dirname+'./frontend/build/index.html',(err) => res.status.send(err)
-  )
-})
 
 // require db connection
 require('./config/db.con');
